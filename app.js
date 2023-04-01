5d80c66cf219d822b9d8ede2578ffdfe4a5858ec
KolbeinnTDK <kolliingi@gmail.com>
2023-04-01 20:20:17 +0000

    name, and list fix

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ document.getElementById("randomize").addEventListener("click", (e) => {
     let numCompetitorsPerLobby = Math.ceil(competitors.length / numLobbies)
 
     competitors = shuffle(competitors);
-    competitors = [0,1,2,3,4,5,6,7,8,9,10,11,12]
 
     var groups = {};
 
